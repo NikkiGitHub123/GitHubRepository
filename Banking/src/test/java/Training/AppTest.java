ec8f4fab1714d63661dba60585c710d64495a1d5
NikkiGitHub123 <nikki.davda@yahoo.com>
2015-11-17 12:11:52 -0800

    First commit of Banking project
    Dumping Banking project

---
+++ Banking/src/test/java/Training/AppTest.java
@@ -0,0 +1,47 @@
+package Training;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @BeforeMethod
+	public void UseridGeneration()
+	{
+		System.out.println("This block executes before each Test");
+	}
+	@BeforeTest
+	public void Cookies()
+	{
+		System.out.println("This block executes Before all  Testcases");
+		//delete cookies
+	}
+@AfterTest
+public void Cookiesclose()
+{
+	System.out.println("This block executes after all  Testcases");
+	//close the browesers
+}
+@AfterMethod
+public void Reportadding()
+{
+	System.out.println("This block executes after each Test");
+}
+
+@Test
+@Parameters()
+public void FlightBooking()
+{
+	
+	System.out.println("Executing Test 1");
+}
+
+}
